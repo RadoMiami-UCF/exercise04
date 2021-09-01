9fc2e471694bf21a3846c5c70a57983c0ed7d860
RadoMiami <kguthre@gmail.com>
2021-09-01 13:07:47 -0400

    Initial commit

---
+++ src/main/java/Solution04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution04 {
+    public static void main(String[] args) {
+        /*
+         *  UCF COP3330 Fall 2021 Assignment 4 Solution
+         *  Copyright 2021 Kimari Guthre
+         */
+        /* Ask for a word, then store it
+        Repeat three times, (can't be looped due to different prints)
+        Print the mad lib story with the four words
+         */
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a noun: ");
+        String noun = in.nextLine();
+        System.out.print("Enter a verb: ");
+        String verb = in.nextLine();
+        System.out.print("Enter an adjective: ");
+        String adjective = in.nextLine();
+        System.out.print("Enter an adverb: ");
+        String adverb = in.nextLine();
+        System.out.printf("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
+    }
+}
