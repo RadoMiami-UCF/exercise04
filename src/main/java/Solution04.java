da70521f2a6885271f71ddf97cc8bf1a7364932b
RadoMiami <kguthre@gmail.com>
2021-09-05 10:38:00 -0400

    Copyright fix

--- src/main/java/Solution04.java
+++ src/main/java/Solution04.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Solution04 {
     public static void main(String[] args) {
         /*
-         *  UCF COP3330 Fall 2021 Assignment 4 Solution
+         *  UCF COP3330 Fall 2021 Assignment 1 Solution
          *  Copyright 2021 Kimari Guthre
          */
         /* Ask for a word, then store it
